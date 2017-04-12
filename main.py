68a65d2b10b0b2681c1706699b861aabba321b83
Niharika <niharikadhawan15@gmail.com>
2017-04-12 19:17:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-a=int(raw_input("Which script do you want to run?\nEnter 1 for students_to_teacher\n Enter 2 for battleship\n Enter 3 for exam_stats "))
+a=int(raw_input("Which script do you want to run?\nEnter 1 for students_to_teacher\n Enter 2 for battleship\n Enter 3 for exam_stats\n "))
 if a==1:
     import students_to_teacher
 elif a==2:
